34b60124ebee600274b09b9c5ebaf5e6371fc1a9
Vinoth kumar <vinothmoorthy.kumar@gmail.com>
2020-04-28 10:29:51 +0530

    form validation added

--- my-app/src/components/add-edit.js
+++ my-app/src/components/add-edit.js
@@ -14,21 +14,62 @@ const styles = {
   },
 };
 
+const validateForm = (errors) => {
+  let valid = true;
+  console.log('errors',errors)
+  Object.values(errors).forEach(
+    (val) => val.result && (valid = false)
+  );
+  return valid;
+}
+
 class addEdit extends React.Component {
   constructor() {
     super();
-    this.state = { username: 'test', errormessage:false };
+    this.state = {
+      name: '',
+      description: '',
+      errors: {
+        name: {
+          message: false,
+          result: false
+        },
+        description: {
+          message: false,
+          result: false
+        },
+      }
+    };
   }
 
   myChangeHandler = (event) => {
-    console.log('event', event.target.value)
-    this.setState({ username: event.target.value });
+    let name = event.target.name;
+    let value = event.target.value;
+    let errors = this.state.errors;
+
+    switch (name) {
+      case 'name':
+        errors.name.message = value.length < 5 ? 'Full Name must be 5 characters long!' : false;
+        console.log('test',value.length < 5)
+        errors.name.result = errors.name.message ? true : false
+        break;
+      case 'description':
+          errors.description.message = value.length < 5 ? 'Description must be 5 characters long!' : false;
+          errors.description.result = errors.description.message ? true : false
+          break;
+      default:
+        break;
+    }
+    this.setState({errors, [name]: value});
   }
 
   mySubmitHandler = (event) => {
     event.preventDefault();
-    this.setState({ errormessage: true });
-    console.log('this.state.username',this.state.username);
+    if(validateForm(this.state.errors)) {
+      console.info('Valid Form')
+    }else{
+      console.error('Invalid Form')
+    }
   }
 
   render() {
@@ -37,9 +78,19 @@ class addEdit extends React.Component {
       <div>
         <form onSubmit={this.mySubmitHandler} className={classes.root} autoComplete="off">
           <TextField
-            error={this.state.errormessage}
+            error={this.state.errors.name.result}
             id="outlined-error-helper-text"
             label="Name"
+            name="name"
+            helperText={this.state.errors.name.message}
+            variant="outlined"
+            onChange={this.myChangeHandler}
+          />
+          <TextField
+            error={this.state.errors.description.result}
+            id="outlined-error-helper-text"
+            label="Description"
+            name="description"
             helperText="Incorrect entry."
             variant="outlined"
             onChange={this.myChangeHandler}
